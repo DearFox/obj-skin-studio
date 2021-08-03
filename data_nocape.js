79eecff50e97203aefcc7f1b33bcfc7d0d8e8295
Semyon <mirsem9@gmail.com>
2021-08-03 15:43:24 +0300

    Adding lost files :)

--- data_nocape.js
+++ data_nocape.js
@@ -1,6 +1,6 @@
-class SharedData {
+class SharedDataa {
 
-    static createDefaultBones() {
+    static createDefaultBoness() {
         return [
             {
                 "name": "root",
@@ -17,31 +17,12 @@ class SharedData {
                 "parent": "root",
                 "pivot": [ 0.0, 12.0, 0.0 ]
             },
-            
-            {
-            	"name": "bodyArmor",
-            	"parent": "body",
-            	"pivot": [0, 24, 0]
-            },
 
-            {
-				"name": "bodyArmorOffset",
-				"parent": "body",
-				"pivot": [0, 24, 0]
-			},
-
-            {
-				"name": "belt",
-				"parent": "body",
-				"pivot": [0, 12, 0]
-			},
-			
             {
                 "name": "head",
                 "parent": "body",
                 "pivot": [ 0.0, 24.0, 0.0 ]
             },
-            
 
             {
                 "name": "cape",
@@ -49,7 +30,6 @@ class SharedData {
                 "rotation":[0,180,0],
                 "parent": "body"
             },
-            
             {
                 "name": "hat",
                 "parent": "head",
@@ -60,62 +40,26 @@ class SharedData {
                 "parent": "head",
                 "pivot": [ 0.0, 24.0, 0.0 ]
             },
-
-            {
-				"name": "helmetArmorOffset",
-				"parent": "head",
-				"pivot": [0, 24, 0]
-			},
-			
             {
                 "name": "leftArm",
                 "parent": "body",
                 "pivot": [ -5.0, 22.0, 0.0 ]
             },
-            
             {
                 "name": "leftSleeve",
                 "parent": "leftArm",
                 "pivot": [ -5.0, 22.0, 0.0 ]
             },
-
-            {
-				"name": "leftArmArmor",
-				"parent": "leftArm",
-				"pivot": [5, 22, 0],
-			},
-
-            {
-				"name": "leftArmArmorOffset",
-				"parent": "leftArm",
-				"pivot": [5, 22, 0]
-			},
-			
-			
             {
                 "name": "leftItem",
                 "pivot": [ -6.0, 15.0, 1.0 ],
                 "parent": "leftArm"
             },
-            
             {
                 "name": "rightArm",
                 "parent": "body",
                 "pivot": [ 5.0, 22.0, 0.0 ]
             },
-
-            {
-				"name": "rightArmArmor",
-				"parent": "rightArm",
-				"pivot": [-5, 22, 0],
-			},
-
-			{
-				"name": "rightArmArmorOffset",
-				"parent": "rightArm",
-				"pivot": [-5, 22, 0]
-			},
-			
             {
                 "name": "rightSleeve",
                 "parent": "rightArm",
@@ -135,30 +79,11 @@ class SharedData {
                 "parent": "root",
                 "pivot": [ -1.9, 12.0, 0.0 ]
             },
-            
             {
                 "name": "leftPants",
                 "parent": "leftLeg",
                 "pivot": [ -1.9, 12.0, 0.0 ]
             },
-
-            {
-				"name": "leftLegging",
-				"parent": "leftLeg",
-				"pivot": [1.9, 12, 0],
-			},
-
-			{
-				"name": "leftLegArmorOffset",
-				"parent": "leftLeg",
-				"pivot": [1.9, 12, 0]
-			},
-			
-			{
-				"name": "leftBootArmorOffset",
-				"parent": "leftLeg",
-				"pivot": [1.9, 12, 0]
-			},
 
             {
                 "name": "rightLeg",
@@ -170,23 +95,6 @@ class SharedData {
                 "parent": "rightLeg",
                 "pivot": [ 1.9, 12.0, 0.0 ]
             },
-
-            {
-				"name": "rightLegging",
-				"parent": "rightLeg",
-				"pivot": [-1.9, 12, 0],
-			},
-
-			{
-				"name": "rightLegArmorOffset",
-				"parent": "rightLeg",
-				"pivot": [-1.9, 12, 0]
-			},
-			{
-				"name": "rightBootArmorOffset",
-				"parent": "rightLeg",
-				"pivot": [-1.9, 12, 0]
-			},
 
             {
                 "name": "jacket",
